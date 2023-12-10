084c969119d7e26fc2622b5d4efac31706d8b076
semtimmy <sem.is@hotmail.nl>
2023-12-10 23:00:05 +0100

    [*] Added comments

--- Day01/Challenge1.cs
+++ Day01/Challenge1.cs
@@ -10,7 +10,8 @@ namespace AdventOfCode
             {
                 string[] lines = File.ReadAllLines("C:/Users/semis/Desktop/AdventOfCode/Day1/AdventOfCode/input.txt");
                 int solution = 0;
-
+				
+				//Fir each line in file, get first and last digit as char, append to eachother, then parse as int. finally add them together
                 for (int i = 0; i < lines.Length; i++)
                 {
                     solution += int.Parse(Char.ToString(GetFirstDigit(lines[i])) + Char.ToString(GetLastDigit(lines[i])));
@@ -24,6 +25,8 @@ namespace AdventOfCode
                 Console.WriteLine(e);
              }
         }
+		
+		//Get first digit in line
         static char GetFirstDigit(string line)
         {
             for (int i = 0; i < line.Length; i += 1)
@@ -32,8 +35,8 @@ namespace AdventOfCode
             }
             return '0';
         }
-
-        
+		
+		//Get last digit in line
         static char GetLastDigit(string line)
         {
             for (int i = line.Length - 1; i >= 0; i -= 1)
